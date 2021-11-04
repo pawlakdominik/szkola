02d854753227330e43e3e3d67759db5b3f43c2cb
pawlakdominik <89968930+pawlakdominik@users.noreply.github.com>
2021-11-04 18:16:10 +0100

    Add files via upload

---
+++ db_select.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Użytkownicy</h4>
+    <?php
+    $connect = new mysqli("localhost", "root", "", "3pi1t");
+    $sql = "SELECT * FROM `users`";
+    $result=$connect->query($sql);
+    //$user=$result->fetch_assoc();//tworzy tablice asocjacyjną
+    //print_r($user);
+
+while ($user=$result->fetch_assoc()) {
+    echo <<< USER
+    Id: $user[id]<br>
+    Imię i nazwisko: $user[name] $user[surname]
+    <hr>
+USER;
+}
+?>
+</body>
+</html>
